35406c544670c3990ce8a3b615085f348664f516
cd <cd@desk.com>
2022-05-01 22:13:23 -0700

    smooth move??

--- code/code.js
+++ code/code.js
@@ -1,10 +1,14 @@
 // display
-const DIMX = 30 ;
-const DIMY = 16 ;
+const DIMX = 8 ; // 30 for 1080p
+const DIMY = 8 ; // 16 for 1080p
 const PIXS = 64 ;
-const SEED = Date.now()
+const SEED = Date.now() ;
+const bnds = ["KeyW", "KeyA", "KeyS", "KeyD", "ArrowUp", "ArrowLeft", "ArrowDown", "ArrowRight"] ;
 // environment setup
-document.addEventListener('keydown', logKey) ;
+document.addEventListener('keydown', down) ;
+document.addEventListener('keyup',   lift) ;
+setInterval(tick, 1000 / 16) ;
+var keys = [] ;
 
 // zone 
 var posx = 0 ;
@@ -17,36 +21,61 @@ var clvl = 0 ;
 // avatar attributes
 var sped = 8 ;
 
-function logKey(e) {
+function tick() {
 	if (!clvl) {
 		zpos = levl() ; 
 		clvl = 1 ;
-	} else {
-		move(`${e.code}` ) ;
-		zpos = posx + "px, " + posy + "px) ; \" class=\"row\">" ;
 	}
-	document.getElementById("dbug").innerHTML = posx + " " + posy ;
+	move()
+	zpos = posx + "px, " + posy + "px) ; \" class=\"row\">" ;
+	document.getElementById("dbug").innerHTML = "keys: " + keys ;
 	document.getElementById("zone").innerHTML = zpre() + zpos + zsuf ;
+	return ;
+}
+
+function down(e) {
+	if ( (keys.indexOf(`${e.code}`) < 0) && (bnds.indexOf(`${e.code}`) > -1) ) {
+		keys.push(`${e.code}`) ;
+	}
+	return ;
+}
+
+function lift(e) {
+	indx = keys.indexOf(`${e.code}`) ;
+	if ( indx > -1 ) {
+		keys.splice(indx,1) ;
+	}
+	return ;
 }
 
 function move(k) {
 	newx = posx ;
 	newy = posy ;
-	if (k == "KeyW" || k == "ArrowUp") {
+	
+	// check current keys
+	inds = [keys.indexOf("KeyW"), keys.indexOf("KeyA"), keys.indexOf("KeyS"), keys.indexOf("KeyD")] ;
+	up__ = (inds[0] > -1 && (inds[0] < inds[2] || inds[2] < 0)) ;
+	left = (inds[1] > -1 && (inds[1] < inds[3] || inds[3] < 0)) ;
+	down = (inds[2] > -1 && !up__) ;
+	rite = (inds[3] > -1 && !left) ;
+	
+	// perform move
+	if (up__) {
 		newy += sped ;
 	}
-	if (k == "KeyA" || k == "ArrowLeft") {
+	if (left) {
 		newx += sped ;
 	}
-	if (k == "KeyS" || k == "ArrowDown") {
+	if (down) {
 		newy -= sped ;
 	}
-	if (k == "KeyD" || k == "ArrowRight") {
+	if (rite) {
 		newx -= sped ;
 	}
 	// we could bounds check here
 	posx = newx ;
 	posy = newy ;
+	return ;
 }
 
 
